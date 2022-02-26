e682e24df0ad5fbe0244ce1c0d54bbdb8ffc781b
tilek <moldaliev47@gmail.com>
2022-02-26 21:05:08 +0600

    some changes

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-
-const app = express()
-
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
-
-app.get(/.*/, function (req,res){
-  res.sendfile(path.join(__dirname,'/dist/index.html'))
-})
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`server started: ${port}`)
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 3000;
+app.listen(port);
