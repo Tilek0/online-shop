85f875a53df24019936008776382f818a87a9c53
tilek <moldaliev47@gmail.com>
2022-03-01 20:03:32 +0600

    change package.json

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const jsonRouter = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(jsonRouter);
-server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT || 4000, () => {
+    console.log('JSON Server is running')
+})
